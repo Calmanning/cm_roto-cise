0afa0240241af84d981550a9f3a134b0b2b2a6b9
That Guy Cal <calmanning@gmail.com>
2021-02-09 09:07:27 -0800

    get and post routes added. Adding Update route now.

--- server.js
+++ server.js
@@ -30,7 +30,85 @@ app.get('/', (req,res) => {
     // res.sendFile('./index.html')
 })
 
+//SEED Test
+const seedWorkouts = [
+    {
+    name: "pushes",
+    type: "Strength",
+    weight: 20,
+    sets: 3,
+    reps: 30,
+    isCardio: false,
+    duration: null
+    },
+    {
+        name: "Sits",
+        type: "Strength",
+        weight: 20,
+        sets: 3,
+        reps: 30,
+        isCardio: false,
+        duration: null
+    },
+    {
+        name: "Runnings",
+        type: "Cardio",
+        weight: null,
+        sets: null,
+        reps: null,
+        isCardio: true,
+        duration: 30
+    },
+    
+]
 
+
+//viewing exercises
+app.get("/api/exercises", (req, res) => {
+    db.Exercise.find({}).then(dbExercise => {
+        res.json(dbExercise)
+    })
+});
+
+//viewing workouts
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({}).then(dbWorkout => {
+        res.json(dbWorkout)
+    })
+});
+
+//populated exercises
+app.get("/popexercise", (req,res) => {
+    db.Workout.find({}).populate("Exercises")
+    .then(dbWorkout => {
+        res.json(dbWorkout)
+    })
+    .catch(err => {
+        console.log(err);
+        res.send(err)
+    })
+})
+
+//adding excercise
+app.post("/api/new-exercise", (req, res) => {
+    console.log(req.body);
+
+    db.Exercise.create(req.body).then(dbExercise => {
+        db.Workout.findOneAndUpdate({_id:req.body.workoutId}, {$push: {exercises: dbExercise._id}})
+        .then(dbWorkout => res.send(dbWorkout))
+    })
+    .catch(err => res.json(err))
+})
+
+//adding workout
+app.post("/api/new-workout", ({ body }, res) => {
+    db.Workout.create(body).then(dbWorkout => {
+        res.json(dbWorkout)
+            }) .catch(err => {
+                console.log(err)
+                res.send(err);
+            })
+})
 
 // =============================================================================
 // LISTENER
