03619199a4debc0dc76b8a52da8170364a76bd73
That Guy Cal <calmanning@gmail.com>
2021-02-09 22:46:46 -0800

    trying the heroku deploy

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(router);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
     useNewUrlParser: true,
     useFindAndModify: false,
+    useCreateIndex: true,
     useUnifiedTopology: true
 });
  
